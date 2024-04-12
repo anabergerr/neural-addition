8b050d08388c8d77f37f44d7107d018026ee1a08
Ana Laura <89489383+anabergerr@users.noreply.github.com>
2024-04-12 00:15:12 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+data_training = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [10,5], [30,30]])
+labels_training = np.array([3, 5, 7, 9, 15,60])
+
+model = models.Sequential([
+    layers.Dense(64, activation='relu', input_shape=(2,)),
+    layers.Dense(1)  # Não especificando a função de ativação para permitir que o modelo aprenda qualquer função
+])
+
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['mean_absolute_error'])
+
+model.fit(data_training, labels_training, epochs=1000, verbose=0)
+
+# Testando o modelo com um novo dado
+prev = model.predict(np.array([[500, 20]]))
+print(prev)  # Deve imprimir algo próximo de 4 (2 + 2)
